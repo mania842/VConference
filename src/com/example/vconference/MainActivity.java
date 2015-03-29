42f19e84a2aedcb1cc89b1ae985324fcb126acd7
Yong <dev.yong842@gmail.com>
2015-03-29 16:49:56 -0400

    Testing commit

--- src/com/example/vconference/MainActivity.java
+++ src/com/example/vconference/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		System.out.println("what");
+		System.out.println("asdfkl;ajsd;fjka;sldfjk");
 		
 		System.out.println("new line");
 		
