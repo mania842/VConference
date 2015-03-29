e6ea6f9b514b90ba9b6d518c10bec0a22d16166a
Yong <dev.yong842@gmail.com>
2015-03-29 17:03:57 -0400

    Updated new line

--- src/com/example/vconference/MainActivity.java
+++ src/com/example/vconference/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 		System.out.println("asdfkl;ajsd;fjka;sldfjk");
 		
 		System.out.println("edited");
-		System.out.println("okokok");
+		System.out.println("okokok asdflkajsdf;klj");
 		
 	}
 
